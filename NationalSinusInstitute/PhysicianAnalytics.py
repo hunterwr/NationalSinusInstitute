67a0dd8d6723a8b1984948d8f3fed0bf30dc170a
hunterwr <rog19011@byui.edu>
2022-06-28 17:54:25 -0600

    Deleted folder

--- NationalSinusInstitute/PhysicianAnalytics.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-
-@st.cache
-def get_data(name_of_csv):
-    return pd.read_csv(f"{name_of_csv}.csv") #use different data here maybe
-
-df = get_data()
